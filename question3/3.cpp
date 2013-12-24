144cc2ad8edc3cf5ef0b7edd4721f48c1d51dd8e
Nikhil-Shenoy <nikhil_shenoy10@yahoo.com>
2013-12-24 12:10:15 -0500

    Adding problem description, solution, and comments

--- question3/3.cpp
+++ question3/3.cpp
@@ -3,18 +3,27 @@
 #include <vector>
 using namespace std;
 
+// Problem Description: What is the largest prime factor of the number 600851475143?
+
+// Approach: I first want to get the factors of the number that was input. I can do this by dividing the input by each number between 2 and the square root of the input. I can do this because if n = a*b, then both a and b cannot exceed the square root of the input. Every time I find a factor of the input, I put it into vector. Once all the factors are found, I iterate through the vector and remove all the composite numbers. After this is done, the largest prime factor should be at the end of the vector.
+
+//Solution: The largest prime factor of 600851475143 is 6857
 
 int main()
 {
+
+	// Get the input from the user
 	double input;
 	cout << "Enter the input you would like to test: ";
 	cin >> input;
 
+
+	// get the ceiling of the square root of the number
 	int check = ceil(sqrt(input));	
-	vector<int> choices;
+	vector<int> choices; // initialize vector
 	double n = 2;
 
-	while(n != check+1)
+	while(n != check+1) // Loop that stores the factors into the vector
 	{
 		double temp = input/n;
 		double temp2 = floor(temp);
@@ -25,8 +34,7 @@ int main()
 		n++;
 	}	
 
-// insert code to remove all non-prime inputs.
-
+	// loop that removes all the composite factors from the vector
 	for(vector<int>::iterator it = choices.end(); it != choices.begin(); it--)
 	{
 
@@ -42,7 +50,8 @@ int main()
 			choices.erase(it);
 		}
 	}	
-			
+		
+	// displaying the vector just to see what it contains	
 	cout << "These are the contents of the vector: " << endl;	
 	for(vector<int>::iterator it = choices.begin(); it != choices.end(); it++)
 	{
